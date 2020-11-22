0c78668c82447c05673819191a86743ca1c1376f
tlfvr <theo.lefievre@gmail.com>
2020-11-22 22:54:26 +0100

    Preprocessing light

--- src/main/scala/paristech/Preprocessor.scala
+++ src/main/scala/paristech/Preprocessor.scala
@@ -1,9 +1,14 @@
 package paristech
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession, functions}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.functions.{bround, concat, length, lit, lower, udf, when}
+
 
 object Preprocessor {
+
+  Logger.getLogger("org").setLevel(Level.WARN)
 
   def main(args: Array[String]): Unit = {
 
@@ -29,20 +34,96 @@ object Preprocessor {
       .appName("TP Spark : Preprocessor")
       .getOrCreate()
 
-    /*******************************************************************************
+    import spark.implicits._
+
+    /** *****************************************************************************
       *
-      *       TP 2
+      * TP 2
       *
       *       - Charger un fichier csv dans un dataFrame
       *       - Pre-processing: cleaning, filters, feature engineering => filter, select, drop, na.fill, join, udf, distinct, count, describe, collect
       *       - Sauver le dataframe au format parquet
       *
-      *       if problems with unimported modules => sbt plugins update
+      * if problems with unimported modules => sbt plugins update
       *
-      ********************************************************************************/
+      * ****************************************************************************** */
 
-    println("\n")
-    println("Hello World ! from Preprocessor")
-    println("\n")
+    val df: DataFrame = spark
+      .read
+      .option("header", true) // utilise la première ligne du (des) fichier(s) comme header
+      .option("inferSchema", "true") // pour inférer le type de chaque colonne (Int, String, etc.)
+      .csv("/home/tlefievre/MSBGD/Spark/spark_project_kickstarter_2020_2021/data/train_clean.csv")
+
+    println(s"Nombre de lignes : ${df.count}")
+    println(s"Nombre de colonnes : ${df.columns.length}")
+
+    df.show()
+
+    val dfCasted: DataFrame = df
+      .withColumn("goal", $"goal".cast("Int"))
+      .withColumn("deadline", $"deadline".cast("Int"))
+      .withColumn("state_changed_at", $"state_changed_at".cast("Int"))
+      .withColumn("created_at", $"created_at".cast("Int"))
+      .withColumn("launched_at", $"launched_at".cast("Int"))
+      .withColumn("backers_count", $"backers_count".cast("Int"))
+      .withColumn("final_status", $"final_status".cast("Int"))
+
+    dfCasted.printSchema()
+
+    dfCasted
+      .select("goal", "backers_count", "final_status")
+      .describe()
+      .show
+
+    val df2: DataFrame = dfCasted.drop("disable_communication")
+
+    val dfNoFutur: DataFrame = df2.drop("backers_count", "state_changed_at")
+
+    dfCasted.groupBy("disable_communication").count.orderBy($"count".desc).show(100)
+    dfCasted.groupBy("country").count.orderBy($"count".desc).show(100)
+    dfCasted.groupBy("currency").count.orderBy($"count".desc).show(100)
+    dfCasted.select("deadline").dropDuplicates.show()
+    dfCasted.groupBy("state_changed_at").count.orderBy($"count".desc).show(100)
+    dfCasted.groupBy("backers_count").count.orderBy($"count".desc).show(100)
+    dfCasted.select("goal", "final_status").show(30)
+    dfCasted.groupBy("country", "currency").count.orderBy($"count".desc).show(50)
+
+
+    df.filter($"country" === "False")
+      .groupBy("currency")
+      .count
+      .orderBy($"count".desc)
+      .show(50)
+
+    val dfCountry: DataFrame = dfNoFutur
+      .withColumn("country2", when($"country" === "False", $"currency").otherwise($"country"))
+      .withColumn("currency2", when($"country".isNotNull && length($"currency") =!= 3, null).otherwise($"currency"))
+      .drop("country", "currency")
+
+    val dfNoUnix: DataFrame = dfCountry
+      .withColumn(colName = "days_campaign", bround(($"deadline" - $"launched_at") / 86400, scale = 3))
+      .withColumn(colName = "hours_prepa", bround(($"launched_at" - $"created_at") / 3600, scale = 3))
+      .drop("deadline", "launched_at", "created_at")
+
+    val dfTextConcat: DataFrame = dfNoUnix
+      .withColumn("text", concat(lower($"name"), lit(' '), lower($"desc"), lit(" "), lower($"keywords")))
+      .drop("name", "desc", "keywords")
+
+    val dfFillNull: DataFrame = dfTextConcat
+      .withColumn("days_campaign", when($"days_campaign".isNull, -1).otherwise($"days_campaign"))
+      .withColumn("hours_prepa", when($"hours_prepa".isNull, -1).otherwise($"hours_prepa"))
+      .withColumn("country", when($"country2".isNull, "unknown").otherwise($"country2"))
+      .withColumn("currency", when($"currency2".isNull, "unknown").otherwise($"currency2"))
+      .drop("country2","currency2")
+
+    dfFillNull.write.parquet("/home/tlefievre/MSBGD/Spark/spark_project_kickstarter_2020_2021/data/train_clean.parquet")
+
+    dfFillNull.show()
+
+
   }
+
+
+
+
 }
